f7bd5645c699a3030333da4da3890d45e5503be7
Taylor Thompson <ttaythompson@gmail.com>
2019-04-25 15:12:59 -0400

    minor changes to app.js and index.js

--- app.js
+++ app.js
@@ -3,28 +3,32 @@ const morgan = require("morgan")
 const html = require("html-template-tag")
 const index = require('./views/index.js')
 const layout = require('./views/layout.js')
-const { db } = require('./models');
-
+const models = require('./models');
 const app = express()
-
 const PORT = 3000
+const init = async () => {
+  await models.db.sync({force: true})
+  app.listen(PORT, () => {
+    console.log('App listening in port')
+  })
+}
 
 app.use(morgan("dev"));
 app.use(express.static(__dirname + "/stylesheets"));
 app.use(express.urlencoded({ extended: false }));
 
 
-// app.use('/', index)
-
 app.get("/", (req, res) => {
   res.send(layout(''))
 })
 
-app.listen(PORT, () => {
-   console.log('App listening in port')
- })
+init();
 
-db.authenticate().
-then(() => {
-  console.log('connected to the database');
-})
+
+// db.authenticate().
+// then(() => {
+//   console.log('connected to the database');
+// })
+
+// console.log("PAGEEEEEEE HEREEEEEEE", Page)
+// console.log("USSSSSERRRRRR HEREEEEEEE", User)
