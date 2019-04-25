bf1ca38d7f573e2d0222622a4cc9291468a39f20
Nikki Bergamini <nikkibergamini@gmail.com>
2019-04-25 18:04:11 -0400

    stuck on wikipage format

--- routes/wiki.js
+++ routes/wiki.js
@@ -2,9 +2,23 @@ const express = require('express')
 const wikiRouter = express.Router()
 const addPage = require('../views/addPage')
 const { Page } = require("../models");
+const wikiPage = require('../views/wikipage.js')
+
 
 wikiRouter.get('/add', function(req, res, next) {
   res.send(addPage())
+});
+
+wikiRouter.get('/:slug', async (req, res, next) => {
+  try {
+    const entry = await Page.findOne({
+      where: {
+        slug: req.params.slug
+      }
+    })
+    console.log("this is the res.json ", res.json(entry), "this is the entry", entry)
+    wikiPage(res.json(entry),"fake author")
+  } catch (err) { next(err) }
 });
 
 wikiRouter.post('/', async (req, res, next) => {
@@ -17,7 +31,7 @@ wikiRouter.post('/', async (req, res, next) => {
   console.log('This is a view of the page:', page)
   try {
     await page.save();
-    res.redirect('/');
+    res.redirect(`/wiki/${page.slug}`);
   } catch (error) { next(error) }
 });
 
